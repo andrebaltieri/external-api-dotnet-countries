0a039d83e5159991d6cebf78ad783fe0c0e3d184
Andre Baltieri <andrebaltieri@hotmail.com>
2022-06-06 19:03:50 -0300

    Finalizado demo

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using RestCountries;
+using RestSharp;
+
+const string url = "https://restcountries.com/v3.1/name/brazil";
+var client = new RestClient(url);
+var request = new RestRequest();
+var response = await client.GetAsync<List<Root>>(request);
+
+foreach (var item in response)
+    Console.WriteLine(item.Name.Official);
